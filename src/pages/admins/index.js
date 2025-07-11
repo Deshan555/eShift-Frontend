a6b73e47ba3d9d13d39e91cf6c55237679617a50
deshanj-eimsky <deshan.jayashanka@eimsky.com>
2025-07-11 21:14:02 +0530

    Add Admins page with DashboardLayout

---
+++ src/pages/admins/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import DashboardLayout from '../../components/DashboardLayout';
+
+const AdminsPage = () => (
+  <DashboardLayout>
+    <h1>Admins</h1>
+    <p>Manage admins here.</p>
+  </DashboardLayout>
+);
+
+export default AdminsPage;
