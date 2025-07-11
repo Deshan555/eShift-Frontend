0076ecd65dddd4f63b361c4d5e24b626f33ceddb
deshanj-eimsky <deshan.jayashanka@eimsky.com>
2025-07-11 21:14:11 +0530

    Add Assistants page with DashboardLayout

---
+++ src/pages/assistants/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import DashboardLayout from '../../components/DashboardLayout';
+
+const AssistantsPage = () => (
+  <DashboardLayout>
+    <h1>Assistants</h1>
+    <p>Manage assistants here.</p>
+  </DashboardLayout>
+);
+
+export default AssistantsPage;
