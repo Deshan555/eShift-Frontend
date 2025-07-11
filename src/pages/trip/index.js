e825463ec4e7a630400b92df1c6c18ae0a1760a0
deshanj-eimsky <deshan.jayashanka@eimsky.com>
2025-07-11 21:33:39 +0530

    Add Trip page with DashboardLayout

---
+++ src/pages/trip/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import DashboardLayout from '../../components/DashboardLayout';
+
+const TripPage = () => (
+  <DashboardLayout>
+    <h1>Trip</h1>
+    <p>Manage trips here.</p>
+  </DashboardLayout>
+);
+
+export default TripPage;
