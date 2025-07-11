fdb2154ff2d726c1a92aacff6b4ee064e80287e3
deshanj-eimsky <deshan.jayashanka@eimsky.com>
2025-07-11 21:16:46 +0530

    Add Jobs page with DashboardLayout

---
+++ src/pages/jobs/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import DashboardLayout from '../../components/DashboardLayout';
+
+const JobsPage = () => (
+  <DashboardLayout>
+    <h1>Jobs</h1>
+    <p>Manage jobs here.</p>
+  </DashboardLayout>
+);
+
+export default JobsPage;
