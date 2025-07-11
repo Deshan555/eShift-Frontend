06e086f3d6c45600b6cd9ef2f106154713004deb
deshanj-eimsky <deshan.jayashanka@eimsky.com>
2025-07-11 21:33:31 +0530

    Add Lorry page with DashboardLayout

---
+++ src/pages/lorry/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import DashboardLayout from '../../components/DashboardLayout';
+
+const LorryPage = () => (
+  <DashboardLayout>
+    <h1>Lorry</h1>
+    <p>Manage lorries here.</p>
+  </DashboardLayout>
+);
+
+export default LorryPage;
