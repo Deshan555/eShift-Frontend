50fd67f93c8b5d4514d04f19c4abc7c3ee797979
deshanj-eimsky <deshan.jayashanka@eimsky.com>
2025-07-11 21:15:22 +0530

    Add City page with DashboardLayout

---
+++ src/pages/city/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import DashboardLayout from '../../components/DashboardLayout';
+
+const CityPage = () => (
+  <DashboardLayout>
+    <h1>City</h1>
+    <p>Manage cities here.</p>
+  </DashboardLayout>
+);
+
+export default CityPage;
