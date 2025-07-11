b2a4821a2c3d0d7e689142bee115d5eb14f89d1b
deshanj-eimsky <deshan.jayashanka@eimsky.com>
2025-07-11 21:16:32 +0530

    Add Drivers page with DashboardLayout

---
+++ src/pages/drivers/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import DashboardLayout from '../../components/DashboardLayout';
+
+const DriversPage = () => (
+  <DashboardLayout>
+    <h1>Drivers</h1>
+    <p>Manage drivers here.</p>
+  </DashboardLayout>
+);
+
+export default DriversPage;
