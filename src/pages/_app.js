8c764c8d299cd9956137e93e1a84b4e196a05e29
deshanj-eimsky <deshan.jayashanka@eimsky.com>
2025-07-12 23:00:21 +0530

    Add custom styles import to _app.js for enhanced styling

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,7 @@
 import 'antd/dist/reset.css';
 import "@/styles/globals.css";
 import "@/styles/fonts.css";
+import "@/styles/custom.css";
 
 export default function App({ Component, pageProps }) {
   return <Component {...pageProps} />;
