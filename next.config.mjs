0440c4ee0e3bed3b9ba55de3ba72394ed0fedfc1
deshanj-eimsky <deshan.jayashanka@eimsky.com>
2025-07-12 11:00:48 +0530

    Update next.config.mjs to include transpilePackages for Ant Design components

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
+
 const nextConfig = {
+  // output: "standalone",
   reactStrictMode: true,
+  productionBrowserSourceMaps: false,
+  transpilePackages: [
+    'rc-util',
+    "rc-picker",
+    "rc-pagination",
+    "@ant-design/icons-svg"
+  ]
 };
 
 export default nextConfig;
