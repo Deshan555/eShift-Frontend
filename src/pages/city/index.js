f85c05b7588d90e9b9d2483c7d3f02432d3ab1cc
deshanj-eimsky <deshan.jayashanka@eimsky.com>
2025-07-18 17:03:46 +0530

    Refactor CityPage to use dynamic import for CityManagement component, disabling server-side rendering

--- src/pages/city/index.js
+++ src/pages/city/index.js
@@ -1,11 +1,2 @@
-import React from 'react';
-import DashboardLayout from '../../components/DashboardLayout';
-
-const CityPage = () => (
-  <DashboardLayout>
-    <h1>City</h1>
-    <p>Manage cities here.</p>
-  </DashboardLayout>
-);
-
-export default CityPage;
+import dynamic from 'next/dynamic';
+export default dynamic(() => import('./CityManagement'), { ssr: false });
