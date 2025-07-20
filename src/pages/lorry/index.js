5d56e5b96f9d4a3f83382727557e89c81b4ae02c
deshanj-eimsky <deshan.jayashanka@eimsky.com>
2025-07-20 09:52:45 +0530

    Remove commented-out code in lorry index page for cleaner implementation

--- src/pages/lorry/index.js
+++ src/pages/lorry/index.js
@@ -1,11 +1,2 @@
 import dynamic from "next/dynamic";
-
-// import React from 'react';
-// import LorryManagement from './management';
-
-// const LorryPage = () => <LorryManagement />;
-
-// export default LorryPage;
-
-
 export default dynamic(() => import('./management'), { ssr: false });
